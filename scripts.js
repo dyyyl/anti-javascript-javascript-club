51028da2f565c00d358e58fc8ee9fa0f2434d13f
dyyyl <dyyyl@delta.local>
2023-06-10 16:20:56 -0400

    an arrow

--- scripts.js
+++ scripts.js
@@ -9,7 +9,7 @@ utterance.rate = 0.65;
 // According to Web Speech API Errata (E11 2013-10-17), the voice list is loaded async to the page. 
 // An onvoiceschanged event is fired when they are loaded.
 // So, the trick is to set your voice from the callback for that event listener:
-window.speechSynthesis.onvoiceschanged = function() {
+window.speechSynthesis.onvoiceschanged = () => {
   const voices = speechSynthesis.getVoices();
   const zarvox = voices.filter(voice => voice.name === 'Zarvox')[0];
 
